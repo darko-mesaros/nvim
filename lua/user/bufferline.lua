33fe340de2dba57cc841f79323475b40acb46007
darko-mesaros <darko.subotica@gmail.com>
2023-09-15 18:41:47 -0700

    fixed icons

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -15,7 +15,7 @@ bufferline.setup {
     -- as an escape hatch for people who cannot bear it for whatever reason
 	indicator_icon = nil,
     indicator = { style = "icon", icon = "▎"},
-    buffer_close_icon = "",
+    buffer_close_icon = "󰅖",
     modified_icon = "●",
     close_icon = "",
     left_trunc_marker = "",
